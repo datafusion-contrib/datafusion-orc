ae467147d5bdcfa414e1e8504ff39eefe42667e3
WenyXu <wenymedia@gmail.com>
2023-06-23 09:53:33 +0000

    feat: add async arrow reader

---
+++ src/async_arrow_reader.rs
@@ -0,0 +1,216 @@
+use std::fmt::Formatter;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::task::{Context, Poll};
+
+use arrow::datatypes::SchemaRef;
+use arrow::error::ArrowError;
+use arrow::record_batch::RecordBatch;
+use futures::future::BoxFuture;
+use futures::{ready, Stream};
+use futures_util::FutureExt;
+use tokio::io::{AsyncRead, AsyncSeek};
+use tokio::sync::Mutex;
+
+use crate::arrow_reader::column::Column;
+use crate::arrow_reader::{
+    create_arrow_schema, Cursor, NaiveStripeDecoder, Stripe, DEFAULT_BATCH_SIZE,
+};
+use crate::error::Result;
+use crate::proto::StripeInformation;
+use crate::reader::schema::TypeDescription;
+use crate::reader::Reader;
+
+pub type BoxedDecoder = Box<dyn Iterator<Item = Result<RecordBatch>>>;
+
+enum StreamState<T> {
+    /// At the start of a new row group, or the end of the file stream
+    Init,
+    /// Decoding a batch
+    Decoding(BoxedDecoder),
+    /// Reading data from input
+    Reading(BoxFuture<'static, Result<(StripeFactory<T>, Option<Stripe>)>>),
+    /// Error
+    Error,
+}
+
+impl<T> std::fmt::Debug for StreamState<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            StreamState::Init => write!(f, "StreamState::Init"),
+            StreamState::Decoding(_) => write!(f, "StreamState::Decoding"),
+            StreamState::Reading(_) => write!(f, "StreamState::Reading"),
+            StreamState::Error => write!(f, "StreamState::Error"),
+        }
+    }
+}
+
+pub struct StripeFactoryInner<R> {
+    reader: Reader<R>,
+    columns: Arc<Vec<(String, Arc<TypeDescription>)>>,
+    stripe_offset: usize,
+}
+
+impl<R> From<Cursor<R>> for StripeFactory<R> {
+    fn from(c: Cursor<R>) -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(StripeFactoryInner {
+                reader: c.reader,
+                columns: c.columns,
+                stripe_offset: c.stripe_offset,
+            })),
+            is_end: false,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct StripeFactory<R> {
+    inner: Arc<Mutex<StripeFactoryInner<R>>>,
+    is_end: bool,
+}
+
+pub struct ArrowStreamReader<R: AsyncRead + AsyncSeek + Unpin + Send> {
+    factory: Option<StripeFactory<R>>,
+    batch_size: usize,
+    schema_ref: SchemaRef,
+    state: StreamState<R>,
+}
+
+impl<R: AsyncRead + AsyncSeek + Unpin + Send> StripeFactory<R> {
+    pub async fn read_next_stripe_inner(&self, info: StripeInformation) -> Result<Stripe> {
+        let mut inner = self.inner.lock().await;
+
+        let column_defs = inner.columns.clone();
+        let stripe_offset = inner.stripe_offset;
+        inner.stripe_offset += 1;
+
+        Stripe::new_async(&mut inner.reader, column_defs, stripe_offset, info).await
+    }
+
+    pub async fn read_next_stripe(mut self) -> Result<(Self, Option<Stripe>)> {
+        let info = {
+            let inner = self.inner.lock().await;
+            inner.reader.stripe(inner.stripe_offset)
+        };
+
+        if let Some(info) = info {
+            match self.read_next_stripe_inner(info).await {
+                Ok(stripe) => Ok((self, Some(stripe))),
+                Err(err) => Err(err),
+            }
+        } else {
+            self.is_end = true;
+            Ok((self, None))
+        }
+    }
+
+    pub async fn is_end(&self) -> bool {
+        self.is_end
+    }
+}
+
+impl<R: AsyncRead + AsyncSeek + Unpin + Send + 'static> ArrowStreamReader<R> {
+    pub fn new(c: Cursor<R>, batch_size: Option<usize>) -> Self {
+        let batch_size = batch_size.unwrap_or(DEFAULT_BATCH_SIZE);
+        let schema = create_arrow_schema(&c);
+        Self {
+            factory: Some(c.into()),
+            batch_size,
+            schema_ref: schema,
+            state: StreamState::Init,
+        }
+    }
+
+    fn poll_next_inner(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<Result<RecordBatch>>> {
+        loop {
+            match &mut self.state {
+                StreamState::Decoding(decoder) => match decoder.next() {
+                    Some(Ok(batch)) => {
+                        return Poll::Ready(Some(Ok(batch)));
+                    }
+                    Some(Err(e)) => {
+                        self.state = StreamState::Error;
+                        return Poll::Ready(Some(Err(e)));
+                    }
+                    None => self.state = StreamState::Init,
+                },
+                StreamState::Init => {
+                    let factory = self.factory.take().expect("lost factory");
+                    if factory.is_end {
+                        return Poll::Ready(None);
+                    }
+
+                    let fut = factory.read_next_stripe().boxed();
+
+                    self.state = StreamState::Reading(fut)
+                }
+                StreamState::Reading(f) => match ready!(f.poll_unpin(cx)) {
+                    Ok((factory, Some(stripe))) => {
+                        self.factory = Some(factory);
+                        match NaiveStripeDecoder::new(
+                            stripe,
+                            self.schema_ref.clone(),
+                            self.batch_size,
+                        ) {
+                            Ok(decoder) => {
+                                self.state = StreamState::Decoding(Box::new(decoder));
+                            }
+                            Err(e) => {
+                                self.state = StreamState::Error;
+                                return Poll::Ready(Some(Err(e)));
+                            }
+                        }
+                    }
+                    Ok((factory, None)) => {
+                        self.factory = Some(factory);
+                        // All rows skipped, read next row group
+                        self.state = StreamState::Init;
+                    }
+                    Err(e) => {
+                        self.state = StreamState::Error;
+                        return Poll::Ready(Some(Err(e)));
+                    }
+                },
+                StreamState::Error => return Poll::Ready(None), // Ends the stream as error happens.
+            }
+        }
+    }
+}
+
+impl<R: AsyncRead + AsyncSeek + Unpin + Send + 'static> Stream for ArrowStreamReader<R> {
+    type Item = std::result::Result<RecordBatch, ArrowError>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.poll_next_inner(cx)
+            .map_err(|e| ArrowError::ExternalError(Box::new(e)))
+    }
+}
+
+impl Stripe {
+    pub async fn new_async<R: AsyncRead + AsyncSeek + Unpin + Send>(
+        r: &mut Reader<R>,
+        column_defs: Arc<Vec<(String, Arc<TypeDescription>)>>,
+        stripe: usize,
+        info: StripeInformation,
+    ) -> Result<Self> {
+        let footer = Arc::new(r.stripe_footer(stripe).clone());
+
+        let compression = r.metadata().postscript.compression();
+        //TODO(weny): add tz
+        let mut columns = Vec::with_capacity(column_defs.len());
+        for (name, typ) in column_defs.iter() {
+            columns.push(Column::new_async(r, compression, name, typ, &footer, &info).await?);
+        }
+
+        Ok(Stripe {
+            footer,
+            columns,
+            stripe_offset: stripe,
+            info,
+        })
+    }
+}
--- src/reader/metadata.rs
+++ src/reader/metadata.rs
@@ -3,27 +3,13 @@ use std::io::{Read, Seek, SeekFrom};
 use bytes::Bytes;
 use prost::Message;
 use snafu::{OptionExt, ResultExt};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncSeek, AsyncSeekExt};
 
 use crate::error::{self, Result};
 use crate::proto::{CompressionKind, Footer, Metadata, PostScript, StripeFooter};
 use crate::reader::decompress::Decompressor;
 
 const DEFAULT_FOOTER_SIZE: u64 = 16 * 1024;
-
-// see (unstable) Seek::stream_len
-fn stream_len(seek: &mut impl Seek) -> Result<u64> {
-    let old_pos = seek.stream_position().context(error::SeekSnafu)?;
-    let len = seek.seek(SeekFrom::End(0)).context(error::SeekSnafu)?;
-
-    // Avoid seeking a third time when we were already at the end of the
-    // stream. The branch is usually way cheaper than a seek operation.
-    if old_pos != len {
-        seek.seek(SeekFrom::Start(old_pos))
-            .context(error::SeekSnafu)?;
-    }
-
-    Ok(len)
-}
 
 /// The file's metadata.
 #[derive(Debug)]
@@ -34,96 +20,124 @@ pub struct FileMetadata {
     pub stripe_footers: Vec<StripeFooter>,
 }
 
+macro_rules! impl_read_metadata {
+    ($reader:ident $($_await:tt)*) => {
+        {
+            let file_len = {
+                let old_pos = $reader.stream_position()$($_await)*.context(error::SeekSnafu)?;
+                let len = $reader.seek(SeekFrom::End(0))$($_await)*.context(error::SeekSnafu)?;
+
+                // Avoid seeking a third time when we were already at the end of the
+                // stream. The branch is usually way cheaper than a seek operation.
+                if old_pos != len {
+                    $reader.seek(SeekFrom::Start(old_pos))$($_await)*
+                        .context(error::SeekSnafu)?;
+                }
+                len
+            };
+
+            // initial read of the footer
+            let assume_footer_len = if file_len < DEFAULT_FOOTER_SIZE {
+                file_len
+            } else {
+                DEFAULT_FOOTER_SIZE
+            };
+
+            $reader
+                .seek(SeekFrom::End(-(assume_footer_len as i64)))$($_await)*
+                .context(error::SeekSnafu)?;
+            let mut tail_bytes = Vec::with_capacity(assume_footer_len as usize);
+            $reader
+                .take(assume_footer_len)
+                .read_to_end(&mut tail_bytes)$($_await)*
+                .context(error::IoSnafu)?;
+
+            // The final byte of the file contains the serialized length of the Postscript,
+            // which must be less than 256 bytes.
+            let postscript_len = tail_bytes[tail_bytes.len() - 1] as usize;
+            tail_bytes.truncate(tail_bytes.len() - 1);
+
+            // next is the postscript
+            let postscript = PostScript::decode(&tail_bytes[tail_bytes.len() - postscript_len..])
+                .context(error::DecodeProtoSnafu)?;
+            tail_bytes.truncate(tail_bytes.len() - postscript_len);
+
+            // next is the footer
+            let footer_length = postscript.footer_length.context(error::OutOfSpecSnafu {
+                msg: "Footer length is empty",
+            })? as usize; // todo: throw error
+
+            let footer_offset = file_len - footer_length as u64 - postscript_len as u64 - 1;
+
+            $reader
+                .seek(SeekFrom::Start(footer_offset))$($_await)*
+                .context(error::SeekSnafu)?;
+            let mut footer = vec![0; footer_length];
+            $reader
+                .read_exact(&mut footer)$($_await)*
+                .context(error::SeekSnafu)?;
+            let footer = deserialize_footer(&footer, postscript.compression())?;
+
+            // finally the metadata
+            let metadata_length = postscript.metadata_length.context(error::OutOfSpecSnafu {
+                msg: "Metadata length is empty",
+            })? as usize;
+            let metadata_offset =
+                file_len - metadata_length as u64 - footer_length as u64 - postscript_len as u64 - 1;
+
+            $reader
+                .seek(SeekFrom::Start(metadata_offset))$($_await)*
+                .context(error::SeekSnafu)?;
+            let mut metadata = vec![0; metadata_length];
+            $reader.read_exact(&mut metadata)$($_await)*.context(error::IoSnafu)?;
+
+            let metadata = deserialize_footer_metadata(&metadata, postscript.compression())?;
+
+            let mut stripe_footers = Vec::with_capacity(footer.stripes.len());
+
+            let mut scratch = Vec::<u8>::new();
+
+            for stripe in &footer.stripes {
+                let start = stripe.offset() + stripe.index_length() + stripe.data_length();
+                let len = stripe.footer_length();
+                $reader
+                    .seek(SeekFrom::Start(start))$($_await)*
+                    .context(error::SeekSnafu)?;
+
+                scratch.clear();
+                scratch.reserve(len as usize);
+                $reader
+                    .take(len)
+                    .read_to_end(&mut scratch)$($_await)*
+                    .context(error::IoSnafu)?;
+                stripe_footers.push(deserialize_stripe_footer(
+                    &scratch,
+                    postscript.compression(),
+                )?);
+            }
+
+            Ok(FileMetadata {
+                postscript,
+                footer,
+                metadata,
+                stripe_footers,
+            })
+        }
+    };
+}
+
 pub fn read_metadata<R>(reader: &mut R) -> Result<FileMetadata>
 where
     R: Read + Seek,
 {
-    let file_len = stream_len(reader)?;
+    impl_read_metadata!(reader)
+}
 
-    // initial read of the footer
-    let assume_footer_len = if file_len < DEFAULT_FOOTER_SIZE {
-        file_len
-    } else {
-        DEFAULT_FOOTER_SIZE
-    };
-
-    reader
-        .seek(SeekFrom::End(-(assume_footer_len as i64)))
-        .context(error::SeekSnafu)?;
-    let mut tail_bytes = Vec::with_capacity(assume_footer_len as usize);
-    reader
-        .take(assume_footer_len)
-        .read_to_end(&mut tail_bytes)
-        .context(error::IoSnafu)?;
-
-    // The final byte of the file contains the serialized length of the Postscript,
-    // which must be less than 256 bytes.
-    let postscript_len = tail_bytes[tail_bytes.len() - 1] as usize;
-    tail_bytes.truncate(tail_bytes.len() - 1);
-
-    // next is the postscript
-    let postscript = PostScript::decode(&tail_bytes[tail_bytes.len() - postscript_len..])
-        .context(error::DecodeProtoSnafu)?;
-    tail_bytes.truncate(tail_bytes.len() - postscript_len);
-
-    // next is the footer
-    let footer_length = postscript.footer_length.context(error::OutOfSpecSnafu {
-        msg: "Footer length is empty",
-    })? as usize; // todo: throw error
-
-    let footer_offset = file_len - footer_length as u64 - postscript_len as u64 - 1;
-
-    reader
-        .seek(SeekFrom::Start(footer_offset))
-        .context(error::SeekSnafu)?;
-    let mut footer = vec![0; footer_length];
-    let _ = reader.read_exact(&mut footer);
-    let footer = deserialize_footer(&footer, postscript.compression())?;
-
-    // finally the metadata
-    let metadata_length = postscript.metadata_length.context(error::OutOfSpecSnafu {
-        msg: "Metadata length is empty",
-    })? as usize;
-    let metadata_offset =
-        file_len - metadata_length as u64 - footer_length as u64 - postscript_len as u64 - 1;
-
-    reader
-        .seek(SeekFrom::Start(metadata_offset))
-        .context(error::SeekSnafu)?;
-    let mut metadata = vec![0; metadata_length];
-    reader.read_exact(&mut metadata).context(error::IoSnafu)?;
-
-    let metadata = deserialize_footer_metadata(&metadata, postscript.compression())?;
-
-    let mut stripe_footers = Vec::with_capacity(footer.stripes.len());
-
-    let mut scratch = Vec::<u8>::new();
-
-    for stripe in &footer.stripes {
-        let start = stripe.offset() + stripe.index_length() + stripe.data_length();
-        let len = stripe.footer_length();
-        reader
-            .seek(SeekFrom::Start(start))
-            .context(error::SeekSnafu)?;
-
-        scratch.clear();
-        scratch.reserve(len as usize);
-        reader
-            .take(len)
-            .read_to_end(&mut scratch)
-            .context(error::IoSnafu)?;
-        stripe_footers.push(deserialize_stripe_footer(
-            &scratch,
-            postscript.compression(),
-        )?);
-    }
-
-    Ok(FileMetadata {
-        postscript,
-        footer,
-        metadata,
-        stripe_footers,
-    })
+pub async fn read_metadata_async<R>(reader: &mut R) -> Result<FileMetadata>
+where
+    R: AsyncRead + AsyncSeek + Unpin + Send,
+{
+    impl_read_metadata!(reader.await)
 }
 
 fn deserialize_footer(bytes: &[u8], compression: CompressionKind) -> Result<Footer> {
