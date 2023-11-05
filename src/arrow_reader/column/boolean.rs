421c92e117a4dc87fe3a7b0fdf42df1ef63bfff1
WenyXu <wenymedia@gmail.com>
2023-11-05 11:15:59 +0000

    feat: support to struct datatype

--- src/arrow_reader/column/boolean.rs
+++ src/arrow_reader/column/boolean.rs
@@ -2,17 +2,18 @@ use snafu::OptionExt;
 
 use crate::arrow_reader::column::present::new_present_iter;
 use crate::arrow_reader::column::{Column, NullableIterator};
+use crate::arrow_reader::Stripe;
 use crate::error::{InvalidColumnSnafu, Result};
 use crate::proto::stream::Kind;
 use crate::reader::decode::boolean_rle::BooleanIter;
 
-pub fn new_boolean_iter(column: &Column) -> Result<NullableIterator<bool>> {
-    let present = new_present_iter(column)?.collect::<Result<Vec<_>>>()?;
+pub fn new_boolean_iter(column: &Column, stripe: &Stripe) -> Result<NullableIterator<bool>> {
+    let present = new_present_iter(column, stripe)?.collect::<Result<Vec<_>>>()?;
     let rows: usize = present.iter().filter(|&p| *p).count();
 
-    let iter = column
-        .stream(Kind::Data)
-        .transpose()?
+    let iter = stripe
+        .stream_map
+        .get(column, Kind::Data)
         .map(|reader| {
             Box::new(BooleanIter::new(reader, rows))
                 as Box<dyn Iterator<Item = Result<bool>> + Send>
--- src/arrow_reader/column/int.rs
+++ src/arrow_reader/column/int.rs
@@ -2,16 +2,17 @@ use snafu::OptionExt;
 
 use crate::arrow_reader::column::present::new_present_iter;
 use crate::arrow_reader::column::{Column, NullableIterator};
+use crate::arrow_reader::Stripe;
 use crate::error::{InvalidColumnSnafu, Result};
 use crate::proto::stream::Kind;
 use crate::reader::decode::get_direct_signed_rle_reader;
 
-pub fn new_i64_iter(column: &Column) -> Result<NullableIterator<i64>> {
-    let present = new_present_iter(column)?.collect::<Result<Vec<_>>>()?;
+pub fn new_i64_iter(column: &Column, stripe: &Stripe) -> Result<NullableIterator<i64>> {
+    let present = new_present_iter(column, stripe)?.collect::<Result<Vec<_>>>()?;
 
-    let iter = column
-        .stream(Kind::Data)
-        .transpose()?
+    let iter = stripe
+        .stream_map
+        .get(column, Kind::Data)
         .map(|reader| get_direct_signed_rle_reader(column, reader))
         .context(InvalidColumnSnafu { name: &column.name })??;
 
