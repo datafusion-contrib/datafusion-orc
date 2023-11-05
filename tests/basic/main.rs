60bd3e1d6f02d2142f68ba9082b0ac90e93fbade
Jefffrey <22608443+Jefffrey@users.noreply.github.com>
2023-11-05 21:50:14 +1100

    Fix bug where not reading all stripes in file

--- tests/basic/main.rs
+++ tests/basic/main.rs
@@ -269,11 +269,10 @@ pub async fn async_basic_test_0() {
 pub fn v0_file_test() {
     let path = basic_path("demo-11-zlib.orc");
     let reader = new_arrow_reader_root(&path);
-    let _expected_row_count = reader.total_row_count();
+    let expected_row_count = reader.total_row_count();
     let batches = reader.collect::<Result<Vec<_>, _>>().unwrap();
-    let _total_rows: usize = batches.iter().map(|b| b.num_rows()).sum();
-    // TODO: not reading entire file, debug
-    // assert_eq!(expected_row_count as usize, total_rows);
+    let total_rows: usize = batches.iter().map(|b| b.num_rows()).sum();
+    assert_eq!(expected_row_count as usize, total_rows);
 }
 
 #[test]
