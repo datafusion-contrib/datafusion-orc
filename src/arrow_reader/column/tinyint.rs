d438c1dd855f2f553507cd3cafcb7fe559298186
WenyXu <wenymedia@gmail.com>
2023-11-09 12:24:32 +0000

    refactor: refactor byte/boolean iter

--- src/arrow_reader/column/tinyint.rs
+++ src/arrow_reader/column/tinyint.rs
@@ -9,14 +9,12 @@ use crate::reader::decode::byte_rle::ByteRleIter;
 
 pub fn new_i8_iter(column: &Column, stripe: &Stripe) -> Result<NullableIterator<i8>> {
     let present = new_present_iter(column, stripe)?.collect::<Result<Vec<_>>>()?;
-    let rows: usize = present.iter().filter(|&p| *p).count();
 
     let iter = stripe
         .stream_map
         .get(column, Kind::Data)
         .map(|reader| {
-            Box::new(ByteRleIter::new(reader, rows).map(|value| value.map(|value| value as i8)))
-                as _
+            Box::new(ByteRleIter::new(reader).map(|value| value.map(|value| value as i8))) as _
         })
         .context(InvalidColumnSnafu { name: &column.name })?;
 
