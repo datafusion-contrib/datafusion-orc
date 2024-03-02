ff8457bbdba40532a3cf0cae29b982b47a3ca368
Jefffrey <jeffrey.vo.australia@gmail.com>
2024-03-02 21:21:37 +1100

    Replace float iter macro with generics

--- src/arrow_reader/column/float.rs
+++ src/arrow_reader/column/float.rs
@@ -3,25 +3,16 @@ use crate::arrow_reader::column::{Column, NullableIterator};
 use crate::arrow_reader::Stripe;
 use crate::error::Result;
 use crate::proto::stream::Kind;
-use crate::reader::decode::float::FloatIter;
+use crate::reader::decode::float::{Float, FloatIter};
 
-macro_rules! impl_float_iter {
-    ($tp:ident) => {
-        paste::item! {
-            pub fn [<new_ $tp _iter>] (column: &Column, stripe: &Stripe) -> Result<NullableIterator<$tp>> {
-                let present = new_present_iter(column, stripe)?.collect::<Result<Vec<_>>>()?;
-                let rows: usize = present.iter().filter(|&p| *p).count();
-                let reader = stripe.stream_map.get(column, Kind::Data)?;
-                let iter = Box::new(FloatIter::<$tp, _>::new(reader, rows));
+pub fn new_float_iter<F: Float>(column: &Column, stripe: &Stripe) -> Result<NullableIterator<F>> {
+    let present = new_present_iter(column, stripe)?.collect::<Result<Vec<_>>>()?;
+    let rows = present.iter().filter(|&p| *p).count();
+    let reader = stripe.stream_map.get(column, Kind::Data)?;
+    let iter = Box::new(FloatIter::<F, _>::new(reader, rows));
 
-                Ok(NullableIterator {
-                    present: Box::new(present.into_iter()),
-                    iter,
-                })
-            }
-        }
-    };
+    Ok(NullableIterator {
+        present: Box::new(present.into_iter()),
+        iter,
+    })
 }
-
-impl_float_iter!(f32);
-impl_float_iter!(f64);
