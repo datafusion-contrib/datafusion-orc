7470406b011f6badabd7b9bcca1b124803197f28
klangner <klangner@gmail.com>
2024-04-16 11:05:29 +0200

    fixed formatting

--- src/bin/orc-stats.rs
+++ src/bin/orc-stats.rs
@@ -5,7 +5,6 @@ use arrow::temporal_conversions::{date32_to_datetime, timestamp_ms_to_datetime};
 use clap::Parser;
 use orc_rust::{reader::metadata::read_metadata, statistics::ColumnStatistics};
 
-
 #[derive(Parser)]
 #[command(name = "orc-stats")]
 #[command(version, about = "Print column and stripe stats from the orc file", long_about = None)]
@@ -24,7 +23,7 @@ fn print_column_stats(col_stats: &ColumnStatistics) {
                 if let Some(sum) = sum {
                     println!("* Sum: {}", sum);
                 }
-            },
+            }
             orc_rust::statistics::TypeStatistics::Double { min, max, sum } => {
                 println!("* Data type Double");
                 println!("* Minimum: {}", min);
@@ -32,23 +31,23 @@ fn print_column_stats(col_stats: &ColumnStatistics) {
                 if let Some(sum) = sum {
                     println!("* Sum: {}", sum);
                 }
-            },
+            }
             orc_rust::statistics::TypeStatistics::String { min, max, sum } => {
                 println!("* Data type String");
                 println!("* Minimum: {}", min);
                 println!("* Maximum: {}", max);
                 println!("* Sum: {}", sum);
-            },
+            }
             orc_rust::statistics::TypeStatistics::Bucket { true_count } => {
                 println!("* Data type Bucket");
                 println!("* True count: {}", true_count);
-            },
+            }
             orc_rust::statistics::TypeStatistics::Decimal { min, max, sum } => {
                 println!("* Data type Decimal");
                 println!("* Minimum: {}", min);
                 println!("* Maximum: {}", max);
                 println!("* Sum: {}", sum);
-            },
+            }
             orc_rust::statistics::TypeStatistics::Date { min, max } => {
                 println!("* Data type Date");
                 if let Some(dt) = date32_to_datetime(*min) {
@@ -57,12 +56,17 @@ fn print_column_stats(col_stats: &ColumnStatistics) {
                 if let Some(dt) = date32_to_datetime(*max) {
                     println!("* Maximum: {}", dt);
                 }
-            },
+            }
             orc_rust::statistics::TypeStatistics::Binary { sum } => {
                 println!("* Data type Binary");
                 println!("* Sum: {}", sum);
-            },
-            orc_rust::statistics::TypeStatistics::Timestamp { min, max, min_utc, max_utc } => {
+            }
+            orc_rust::statistics::TypeStatistics::Timestamp {
+                min,
+                max,
+                min_utc,
+                max_utc,
+            } => {
                 println!("* Data type Timestamp");
                 println!("* Minimum: {}", min);
                 println!("* Maximum: {}", max);
@@ -72,13 +76,17 @@ fn print_column_stats(col_stats: &ColumnStatistics) {
                 if let Some(ts) = timestamp_ms_to_datetime(*max_utc) {
                     println!("* Maximum UTC: {}", ts);
                 }
-            },
-            orc_rust::statistics::TypeStatistics::Collection { min_children, max_children, total_children } => {
+            }
+            orc_rust::statistics::TypeStatistics::Collection {
+                min_children,
+                max_children,
+                total_children,
+            } => {
                 println!("* Data type Collection");
                 println!("* Minimum children: {}", min_children);
                 println!("* Maximum children: {}", max_children);
                 println!("* Total children: {}", total_children);
-            },
+            }
         }
     }
 
@@ -87,7 +95,6 @@ fn print_column_stats(col_stats: &ColumnStatistics) {
     println!("");
 }
 
-
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
@@ -95,7 +102,11 @@ fn main() -> Result<()> {
     let metadata = Arc::new(read_metadata(&mut f)?);
 
     println!("# Column stats");
-    println!("File {:?} has {} columns", cli.file, metadata.column_file_statistics().len());
+    println!(
+        "File {:?} has {} columns",
+        cli.file,
+        metadata.column_file_statistics().len()
+    );
     println!();
     for (idx, col_stats) in metadata.column_file_statistics().iter().enumerate() {
         println!("## Column {idx}");
@@ -103,7 +114,11 @@ fn main() -> Result<()> {
     }
 
     println!("# Stripe stats");
-    println!("File {:?} has {} stripes", cli.file, metadata.stripe_metadatas().len());
+    println!(
+        "File {:?} has {} stripes",
+        cli.file,
+        metadata.stripe_metadatas().len()
+    );
     println!();
     for (idm, sm) in metadata.stripe_metadatas().iter().enumerate() {
         println!("----- Stripe {idm} -----\n");
