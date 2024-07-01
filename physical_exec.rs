05825eaaadc1b44934eb869ee8cba473b29b20c6
Xuanwo <github@xuanwo.io>
2024-07-01 21:19:38 +0800

    refactor: Bump arrow 52 and datafusion 39 (#105)
    * refactor: Bump arrow 52 and datafusion 39
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix union
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Format code
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    ---------
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- physical_exec.rs
+++ physical_exec.rs
@@ -59,7 +59,7 @@ impl ExecutionPlan for OrcExec {
         self
     }
 
-    fn children(&self) -> Vec<Arc<dyn ExecutionPlan>> {
+    fn children(&self) -> Vec<&Arc<dyn ExecutionPlan>> {
         vec![]
     }
 
